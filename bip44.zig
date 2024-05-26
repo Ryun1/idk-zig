7906e33cc16e9108a0caf97214ec40924661a87d
Ryan Williams <ryan.williams@intersectmbo.org>
2024-05-27 00:37:05 +0100

    uh huh

---
+++ bip44.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+const ed25519 = std.crypto.sign.Ed25519;
+const fmt = std.fmt;
+
+// Lets test out some HD wallet implementations
+// https://github.com/iskyd/walle
+//
+
+pub fn main() !void {
+    std.debug.print("Generating\n", {});
+}
--- main.zig
+++ main.zig
@@ -1,21 +1,22 @@
 const std = @import("std");
+const ed25519 = std.crypto.sign.Ed25519;
 const fmt = std.fmt;
 
 // Lets test out some Ed25519 crypto
 // https://github.com/ziglang/zig/blob/master/lib/std/crypto/25519/ed25519.zig
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"keys"});
-
-    try generate_key();
+    std.debug.print("Generating you a random Ed25519 key pair\n", .{});
+    try generate_random_key_pair();
 }
 
-pub fn generate_key() !void {
+pub fn generate_random_key_pair() !void {
     const myRandomKeyPair = try std.crypto.sign.Ed25519.KeyPair.create(null);
     const myPublicKey = myRandomKeyPair.public_key;
     const myPublicKeyBytes = myPublicKey.toBytes();
-    // const mySecretKey = myRandomKeyPair.secret_key;
+    const mySecretKey = myRandomKeyPair.secret_key;
+    const mySecretKeyBytes = mySecretKey.toBytes();
 
-    std.debug.print("hi my pub key is : {s}\n", .{fmt.bytesToHex(myPublicKeyBytes, .lower)});
+    std.debug.print("Random pub key is : {s}\n", .{fmt.bytesToHex(myPublicKeyBytes, .lower)});
+    std.debug.print("Random secret key is : {s}\n", .{fmt.bytesToHex(mySecretKeyBytes, .lower)});
 }
